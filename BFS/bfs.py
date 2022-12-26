49654032f0e6e054c2c22a3c257dbbc60c018b68
Xiaoyu Lyu <lyu.xiaoyu2@mail.dcu.ie>
2022-12-26 17:15:08 +0000

    py

---
+++ BFS/bfs.py
@@ -0,0 +1,34 @@
+from collections import deque
+class Graphy:
+    def __init__(self, edges, n):
+        self.adjList = [[] for _ in range(n)]
+        
+        for (head, leaf) in edges:
+            self.adjList[head].append(leaf)
+            self.adjList[leaf].append(head)
+
+def bfs(graphy, vertex, discovered):
+    q = deque()
+    discovered[vertex] = True
+    q.append(vertex)
+    
+    while q:
+        vertex = q.popleft()
+        print(vertex, end = " ")
+        
+        for u in graphy.adjList[vertex]:
+            if not discovered[u]:
+                discovered[u] = True
+                q.append(u)
+
+if __name__ == "__main__":
+    edges = [(1, 2), (1, 3), (1, 4), (2, 5), (2, 6), (5, 9),
+        (5, 10), (4, 7), (4, 8), (7, 11), (7, 12)]
+    n = 15
+    graphy = Graphy(edges, n)
+    
+    discovered = [False] * n
+    
+    for i in range(n):
+        if not discovered[i]:
+            bfs(graphy, i, discovered)
