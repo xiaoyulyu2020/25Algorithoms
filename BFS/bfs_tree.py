b2b2d58122f0fc21d7b3530e26084c959c3605a5
Xiaoyu Lyu <lyu.xiaoyu2@mail.dcu.ie>
2022-12-29 12:27:49 +0000

    start

--- BFS/bfs_tree.py
+++ BFS/bfs_tree.py
@@ -1,8 +1,29 @@
 
 class Node:
     def __init__(self, value):
-        self.value = value
+        self.value = None
         self.left = None
         self.right = None
 
     def insert(self, value):
+        if self.value == None:
+            self.value = value
+        else:
+            if value >= self.value:
+                self.right = self.insert(value)
+            else:
+                self.left = self.insert(value)
+    
+def getHeight(node):
+    if node.value is None:
+        return 0
+    else:
+        leftHeight = getHeight(node.left)
+        rightHeight = getHeight(node.right)
+        if leftHeight > rightHeight:
+            return leftHeight + 1
+        else:
+            return rightHeight + 1
+
+def currentLevelOut(root, level):
+    if root is 
